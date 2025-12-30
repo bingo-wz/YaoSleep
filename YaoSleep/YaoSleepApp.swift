947ebbe5b9d2265d4f9cd587636a814574c8579c
bingo <2248941022@qq.com>
2025-12-30 17:40:15 +0800

    Initial Commit

---
+++ YaoSleep/YaoSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaoSleepApp.swift
+//  YaoSleep
+//
+//  Created by 小志 on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct YaoSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
